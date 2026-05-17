75c0dd958422f6f58b9da97974a55f3a744c8b14
koharu <koharudotgit@duck.com>
2026-05-18 06:23:03 +0700

    feat: initial commit

---
+++ lua/plugins/42norminette.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "Chayanon-Ninyawee/norminette42.nvim",
+    opts = {
+      user = "kpiwan",                       -- your 42 username
+      mail = "kpiwan@student.42bangkok.com", -- your 42 email
+      -- add other options.
+    },
+  },
+}
